96ff4747a840d217f066902973e5daa5fb875971
Stivenson Rincón <stivenson.rpm@gmail.com>
2009-11-12 04:46:38 +0000

    refactor: add file /Users/stivenson/work/Documents/projects/personal-old/programas-java/Programas-java-por-consola/Programas-primer-semestre/Copia de longitud.java

---
+++ Programas-java-por-consola/Programas-primer-semestre/Copia de longitud.java
@@ -0,0 +1,29 @@
+class logitud
+{
+	public static void main(String arg[])
+	{
+		int x1,y1,x2,y2;
+		double re;
+		
+		System.out.print("X1: ");
+ 	    x1=Leer.leerint();
+ 	    System.out.print("y1: ");
+ 	    y1=Leer.leerint();
+ 	    System.out.print("X2: ");
+ 	    x2=Leer.leerint();
+ 	    System.out.print("y2: ");
+ 	    y2=Leer.leerint();
+ 	    
+ 	    
+ 	    re=Math.pow(Math.pow((x2-x1),2)+Math.pow((y2-y1),2),0.5);
+ 	    
+ 	    if(re==0)
+ 	    {
+ 	    System.out.print("longitud: 0");	
+ 	    }
+ 	    else
+ 	    {
+ 	    System.out.print("longitud: "+re);
+ 	    }
+	}
+}
