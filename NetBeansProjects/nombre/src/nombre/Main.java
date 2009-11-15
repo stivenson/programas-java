b51dfb6810d3932c80422eee0d0b2ab3af1ce979
Stivenson Rincón <stivenson.rpm@gmail.com>
2009-11-15 23:15:38 +0000

    refactor: add file /Users/stivenson/work/Documents/projects/personal-old/programas-java/NetBeansProjects/nombre/src/nombre/Main.java

---
+++ NetBeansProjects/nombre/src/nombre/Main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package nombre;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Stivenson
+ */
+public class Main {
+   
+
+
+
+
+public static void main(String[] args) {
+JOptionPane.showMessageDialog(null,"STIVENSON" );
+}
+
+}
+     
+        
+    
+
