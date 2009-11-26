2f7390e2ea6123b6e786eecc0b6a000a4039c7c0
Stivenson Rincón <stivenson.rpm@gmail.com>
2009-11-26 03:28:58 +0000

    refactor: add file /Users/stivenson/work/Documents/projects/personal-old/programas-java/Programas-java-por-consola/Programas-primer-semestre/ciclo_acumulador.java

---
+++ Programas-java-por-consola/Programas-primer-semestre/ciclo_acumulador.java
@@ -0,0 +1,50 @@
+class ciclo_acumulador
+{
+		public static void main(String arg[])
+		{
+		int pasadas=0,i,b=1;
+		double c=0,ne=0,n,p=1.5,cn=0,a=0;
+		String vs="";
+		
+			System.out.print("N: ");
+ 	        n=Leer.leerint();
+ 	       
+ 		
+		
+		System.out.print("0, ");
+		for(i=0;i<=n-2;i++)
+		{
+		ne=ne+i+0.5	;
+		System.out.print(ne+", ");
+		
+	    
+	    
+	    	
+	    
+	   
+	   
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+     	
+     	
+     	
+        }
+		
+		}
+}
