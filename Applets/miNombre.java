7fb6e759c5013c3b538196ca0b83e62b69a55857
Stivenson Rincón <stivenson.rpm@gmail.com>
2010-02-04 22:11:16 +0000

    refactor: add file /Users/stivenson/work/Documents/projects/personal-old/programas-java/Applets/miNombre.java

---
+++ Applets/miNombre.java
@@ -0,0 +1,21 @@
+/**
+ * @(#)miNombre.java
+ *
+ * @author Stivenson Rincón Móra
+ * @version 1.00 2010/2/4
+ */
+
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class miNombre extends java.applet.Applet {
+	
+
+ 
+    public void paint(Graphics objeto) {
+             objeto.drawString("Stivenson Rincón Móra", 30, 60);
+             objeto.drawRect(20,37,150,37);
+    }
+}
+   
+
