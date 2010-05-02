6d3852a1a2406182a68d9283e91a6579147be0c7
Stivenson Rincón <stivenson.rpm@gmail.com>
2010-05-02 22:17:08 +0000

    refactor: add file /Users/stivenson/work/Documents/projects/personal-old/programas-java/Programas-java-por-consola/Programas-Segundo-semestre/Random.java

---
+++ Programas-java-por-consola/Programas-Segundo-semestre/Random.java
@@ -0,0 +1,24 @@
+/**
+ * @(#)Random.java
+ *
+ *
+ * @author 
+ * @version 1.00 2010/5/2
+ */
+
+public class Random {
+        
+    /**
+     * Creates a new instance of <code>Random</code>.
+     */
+    public Random() {
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for(int i=0;i<32;i++)
+        	System.out.println((char)(i));
+    }
+}
