466c2f1ae67a1714c53d0fe696d941cd3e7edf9a
Stivenson Rincón <stivenson.rpm@gmail.com>
2010-05-12 23:00:42 +0000

    refactor: add file /Users/stivenson/work/Documents/projects/personal-old/programas-java/Programas-java-por-consola/Programas-Segundo-semestre/Clase_string.java

---
+++ Programas-java-por-consola/Programas-Segundo-semestre/Clase_string.java
@@ -0,0 +1,28 @@
+/**
+ * @(#)Clase_string.java
+ *
+ *
+ * @author 
+ * @version 1.00 2010/5/12
+ */
+
+class Programa2 {
+    public static void main(String[] args) {
+    
+    
+    
+    } 
+   
+}
+
+
+
+ /* System.out.print("Escriba su frase: ");
+     String c1=Leer.leercadena(); 
+     System.out.print("Escriba la palabra: ");
+     String c2=Leer.leercadena();
+     if(c1.toLowerCase().indexOf(c2.toLowerCase())>=0)
+     	System.out.println("Si esta"); 
+     	else
+     	System.out.println("No esta"); 	
+    */
